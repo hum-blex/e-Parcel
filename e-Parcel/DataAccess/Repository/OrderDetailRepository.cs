5c19ab9df803b4ed6f6572b7a29fd6cabd5e1a2a
xdGTH <gotohell.heaven45@gmail.com>
2024-03-26 13:41:53 +0545

    Added Order Details Controller

--- e-Parcel/DataAccess/Repository/OrderDetailRepository.cs
+++ e-Parcel/DataAccess/Repository/OrderDetailRepository.cs
@@ -12,6 +12,12 @@ namespace e_Parcel.DataAccess.Repository
 		}
 		public void Update(OrderDetail obj)
 		{
+			var existingOrder = _db.OrderDetails.FirstOrDefault(x => x.Id == obj.Id);
+
+			existingOrder.UserId = obj.UserId;
+			existingOrder.PaymentDetails = obj.PaymentDetails;
+			existingOrder.PaymentId = obj.PaymentId;
+
 			_db.OrderDetails.Update(obj);
 		}
 	}
